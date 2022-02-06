4f190ee69a2233bbcc3e8835c65cff18c21e0692
Flutterappuser <programeur2017@gmail.com>
2022-02-06 12:00:24 +0100

    fittedbox

--- lib/hompage.dart
+++ lib/hompage.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class Hompage extends StatefulWidget {
@@ -8,45 +10,24 @@ class Hompage extends StatefulWidget {
 }
 
 class _HompageState extends State<Hompage> {
-  int _currentStep = 0 ;
+
   @override
   Widget build(BuildContext context) {
 
     return  Scaffold(
         appBar:  AppBar(title: const Text("Test")),
         body: Center(
-          child: Stepper(steps: const [
-            Step(title: Text('Step 1'),
-            content: Text('Information for step 1')),
-            Step(title: Text('Step 2'),
-                content: Text('Information for step 2')),
-            Step(title: Text('Step 3'),
-                content: Text('Information for step 3')),
-          ],
-          onStepTapped: (int newindex) {
-            setState(() {
-              _currentStep = newindex ;
-            });
-          },
-
-            currentStep: _currentStep,
-
-            onStepContinue: (){
-            if (_currentStep != 2){
-              setState(() {
-                _currentStep +=1;
-              });
-            }
-            },
-
-            onStepCancel: (){
-              if (_currentStep != 0){
-                setState(() {
-                  _currentStep -=1;
-                });
-              }
-            },
-
+          child: Container(
+            width: 300,
+            height: 150,
+            color: Colors.red,
+            padding: const EdgeInsets.all(10.0),
+            child: const FittedBox(
+              child:  Text('FLUTTER APP',
+              style: TextStyle(fontSize: 100,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,),),
+            ),
           ),
         ),
 
